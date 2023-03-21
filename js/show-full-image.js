7786c951612c525e56a14686ff125ad60a4725ee
SvenMayCry <svenmaycry@gmail.com>
2023-03-21 12:45:58 +0300

    add: renderComments

--- js/show-full-image.js
+++ js/show-full-image.js
@@ -1,38 +1,7 @@
 import { createCommentItem } from './create-comment.js';
 import { isEscapeKey } from './util.js';
 
-const openFullImage = (item, data) => {
-  item.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    document.querySelector('body').classList.add('modal-open');
-    // document.querySelector('.social__comment-count').classList.add('hidden');
-    // document.querySelector('.comments-loader').classList.add('hidden');
-    document.querySelector('.big-picture').classList.remove('hidden');
-    document.querySelector('.big-picture .big-picture__img img').setAttribute('src', data.url);
-    document.querySelector('.big-picture .likes-count').textContent = data.likes;
-    document.querySelector('.big-picture .comments-count').textContent = data.comments.length;
-    document.querySelector('.big-picture .social__caption').textContent = data.description;
-    document.querySelector('.social__comments').innerHTML = '';
-    data.comments.forEach((comment) => document.querySelector('.social__comments').append(createCommentItem(comment)));
-  });
-};
-
-const closeFullImage = () => {
-  const closeButton = document.querySelector('.big-picture__cancel');
-  window.addEventListener('keydown', (evt) => {
-    if (isEscapeKey(evt)) {
-      document.querySelector('.big-picture').classList.add('hidden');
-      document.querySelector('body').classList.remove('modal-open');
-    }
-  });
-  closeButton.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    document.querySelector('.big-picture').classList.add('hidden');
-    document.querySelector('body').classList.remove('modal-open');
-  });
-};
-
-
+//!!! Все данные для renderComments
 const VISIBLE_COMMENTS = 5;
 const commentsList = document.querySelector('.social__comments');
 const commentsLoader = document.querySelector('.comments-loader');
@@ -40,20 +9,18 @@ const commentCount = document.querySelector('.social__comment-count');
 
 let shownComments = 0;
 const comments = [];
-
-const renderComments = () => {
+//!!! Функция загрузки комментов
+const renderComments = (data) => {
   shownComments += VISIBLE_COMMENTS;
-
-  if (shownComments >= comments.length) {
+  if (shownComments >= data.comments.length) {
     commentsLoader.classList.add('hidden');
-    shownComments = comments.length;
+    shownComments = data.comments.length;
   } else {
     commentsLoader.classList.remove('hidden');
   }
-
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < shownComments; i++) {
-    const commentElement = createCommentItem(comments[i]);
+    const commentElement = createCommentItem(data.comments[i]);
     fragment.append(commentElement);
   }
 
@@ -62,4 +29,46 @@ const renderComments = () => {
   commentCount.innerHTML = `${shownComments} из <span class="comments-count">${comments.length}</span>`;
 };
 
-export { openFullImage, closeFullImage, renderComments };
+const openFullImage = (item, data) => {
+  item.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    document.querySelector('body').classList.add('modal-open');
+    document.querySelector('.big-picture').classList.remove('hidden');
+    document.querySelector('.big-picture .big-picture__img img').setAttribute('src', data.url);
+    document.querySelector('.big-picture .likes-count').textContent = data.likes;
+    document.querySelector('.big-picture .comments-count').textContent = data.comments.length;
+    document.querySelector('.big-picture .social__caption').textContent = data.description;
+    document.querySelector('.social__comments').innerHTML = '';
+    //??? Если я сразу вызываю renderComments(data), то при открытии фотографии сразу загружаются 5 комментов, но каждый раз при открытии и закрытии фотографии они прибавляются
+    // renderComments(data);
+    //??? Если я делаю через событие, тогда комменты сразу не показываются и ломается счётчик
+    commentsLoader.addEventListener('click', () => {
+      renderComments(data);
+    });
+  });
+};
+
+const closeFullImage = (data) => {
+  const closeButton = document.querySelector('.big-picture__cancel');
+  window.addEventListener('keydown', (evt) => {
+    if (isEscapeKey(evt)) {
+      document.querySelector('.big-picture').classList.add('hidden');
+      document.querySelector('body').classList.remove('modal-open');
+      //??? Как обновить весь список комментов при закрытии фотографии?
+      commentsLoader.removeEventListener('click', () => {
+        renderComments(data);
+      });
+    }
+  });
+  closeButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    document.querySelector('.big-picture').classList.add('hidden');
+    document.querySelector('body').classList.remove('modal-open');
+    //??? Как обновить весь список комментов при закрытии фотографии?
+    commentsLoader.removeEventListener('click', () => {
+      renderComments(data);
+    });
+  });
+};
+
+export { openFullImage, closeFullImage };
