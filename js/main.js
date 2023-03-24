a03662ada6a7413e4f1ac178d4db45603fa8032f
SvenMayCry <svenmaycry@gmail.com>
2023-03-24 14:28:57 +0300

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,5 @@ import { getSimilarPhotoDescription } from './data.js';
 import { renderThumbnails } from './show-thumbnails.js';
 
 import './edit-form.js';
-import './validate.js';
 
 renderThumbnails(getSimilarPhotoDescription());
