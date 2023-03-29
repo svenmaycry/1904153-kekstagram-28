95ed06076403e343f12d82aef66aa4e3f158bfaa
SvenMayCry <svenmaycry@gmail.com>
2023-03-30 00:04:10 +0300

    add: work with server

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-import { getSimilarPhotoDescription } from './data.js';
 import { renderThumbnails } from './show-thumbnails.js';
+import { getData, sendData } from './api.js';
+import { showAlert } from './util.js';
+import { onFormSubmit, hideModal } from './edit-form.js';
+import { showFullSuccessMessage, showFullErrorMessage } from './edit-form.js';
 
-import './edit-form.js';
+onFormSubmit(async (data) => {
+  try {
+    await sendData(data);
+    hideModal();
+    showFullSuccessMessage();
+  } catch {
+    showFullErrorMessage();
+  }
+});
 
-renderThumbnails(getSimilarPhotoDescription());
+try {
+  const data = await getData();
+  renderThumbnails(data);
+} catch (err) {
+  showAlert(err.message);
+}
